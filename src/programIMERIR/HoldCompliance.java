a4dc3d5e258fbebf1f00acf7e0144fc1d81c689d
Aurélien <Aurlien@DESKTOP-62ON3LS>
2018-03-19 18:08:16 +0100

    --- automatic rebase to remote; old local SHA: 1235426e15fcf7a0cbb9667abc758cb3c6efd0b1 ---

--- src/programIMERIR/HoldCompliance.java
+++ src/programIMERIR/HoldCompliance.java
@@ -109,7 +109,7 @@ public class HoldCompliance extends RoboticsAPIApplication {
 				//penTCP.copyWithRedundancy(robot.getFrame("/WorkingTable/StartingPoint"));
 				//draw();
 				//penTCP.move(linRel(currentFrame.getX(), currentFrame.getY(), -100));
-				penTCP.move(lin(getApplicationData().getFrame("/Foam/P1")));
+				penTCP.move(lin(getApplicationData().getFrame("/WorkingTable/WaitingPoint")));
 			}
 		}
 	};
