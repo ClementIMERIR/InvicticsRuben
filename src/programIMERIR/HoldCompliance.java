c89fd5b88cdc0c39492b3e53321c86f7e389916c
Aurélien <Aurlien@DESKTOP-62ON3LS>
2018-03-20 18:09:54 +0100

    --- automatic rebase to remote; old local SHA: 7023dd4afbb6fb77ee09031bedd3edc27df86452 ---

--- src/programIMERIR/HoldCompliance.java
+++ src/programIMERIR/HoldCompliance.java
@@ -114,7 +114,7 @@ public class HoldCompliance extends RoboticsAPIApplication {
 				//grabForceObserver.disable();
 				penCollisionObserver.enable();
 				RelativeLIN descente = linRel(0,0,-currentFrame.getZ());
-				descente.setCartVelocity(100);
+				descente.setCartVelocity(30);
 				descente.breakWhen(penCollision);
 				penWorldAlign.move(descente);
 			}
