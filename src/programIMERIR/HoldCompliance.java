82982117c5a8b1c307db6416de04c1f4b29274d3
Aurélien <Aurlien@DESKTOP-62ON3LS>
2018-03-20 11:03:25 +0100

    --- automatic rebase to remote; old local SHA: 534feae5b45b70be65d8584af0b3ac8d405a6bb0 ---

--- src/programIMERIR/HoldCompliance.java
+++ src/programIMERIR/HoldCompliance.java
@@ -110,7 +110,7 @@ public class HoldCompliance extends RoboticsAPIApplication {
 				getLogger().info("Button startDrawing triggered");
 				Frame currentFrame = robot.getCurrentCartesianPosition(penWorldAlign);
 				getLogger().info("penInfos = " + currentFrame.getX() + " , " + currentFrame.getY() + " , "  + currentFrame.getZ());
-				grabForceObserver.disable();
+				//grabForceObserver.disable();
 				penCollisionObserver.enable();
 				RelativeLIN descente = linRel(0,0,currentFrame.getZ());
 				//descente.setCartVelocity(0.2);
