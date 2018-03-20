c6eb61c0f561f10a59f91f9bb33495e757ace05b
Clement Bourdarie <clement.bourdarie@imerir.com>
2018-03-20 14:35:20 +0100

    --- automatic rebase to remote; old local SHA: bfd3aa36f59866ef743657388e15691d6806ebef ---

--- src/application/HoldAndDo.java
+++ src/application/HoldAndDo.java
@@ -220,8 +220,8 @@ public class HoldAndDo extends RoboticsAPIApplication {
 		distanceX = maxX - minX;
 		distanceY = maxY - minY;
 		
-		pliers.getFrame("Sander").move(lin(framePoints.get(0)).setJointVelocityRel(0.5));
-		for(int i = 0 ; i < maxX ; i += largeurOutil/2) {
+		//pliers.getFrame("Sander").move(lin(framePoints.get(0)).setJointVelocityRel(0.5));
+		for(int i = 0 ; i < maxY ; i += largeurOutil/2) {
 			pliers.getFrame("Sander").move(linRel(distanceX, 0, 0).setJointVelocityRel(1.0));
 			pliers.getFrame("Sander").move(linRel(-distanceX, 0, 0).setJointVelocityRel(0.5));
 			pliers.getFrame("Sander").move(linRel(0, largeurOutil/2, 0).setJointVelocityRel(0.5));
