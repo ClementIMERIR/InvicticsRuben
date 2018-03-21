c46531bceb7cf723048e72dc041de1dccde5303c
dmBontemps <dmbontemps@github.com>
2018-03-21 11:20:45 +0100

    --- automatic rebase to remote; old local SHA: c47d463c4d6998ed5f07d8786937da25ec890661 ---

--- src/programIMERIR/AllSurfacesDrawing.java
+++ src/programIMERIR/AllSurfacesDrawing.java
@@ -8,7 +8,6 @@ import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
 import static com.kuka.roboticsAPI.motionModel.BasicMotions.*;
 import com.kuka.roboticsAPI.deviceModel.LBR;
 import com.kuka.roboticsAPI.motionModel.PositionHold;
-import com.kuka.roboticsAPI.motionModel.RelativeLIN;
 import com.kuka.roboticsAPI.motionModel.controlModeModel.CartesianImpedanceControlMode;
 
 /**
@@ -32,7 +31,6 @@ import com.kuka.roboticsAPI.motionModel.controlModeModel.CartesianImpedanceContr
 public class AllSurfacesDrawing extends RoboticsAPIApplication {
 	@Inject
 	private LBR robot;
-	private RelativeLIN mouvement;
 	
 	private CartesianImpedanceControlMode freeMoveMode;
 
