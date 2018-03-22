db7bfdb1d1f4ee65f1b32d6c0b353a91b79d5620
rForcee <erwannfrancois@gmail.com>
2018-03-22 18:00:23 +0100

    --- automatic rebase to remote; old local SHA: fd6eebf18ce5d3baa787d22f372bb54dfdaa3554 ---

--- src/application/HoldAndDo.java
+++ src/application/HoldAndDo.java
@@ -280,15 +280,11 @@ public class HoldAndDo extends RoboticsAPIApplication {
 		
 		//robot.move(ptp(JPosition.get(0),JPosition.get(1),JPosition.get(2),JPosition.get(3),JPosition.get(4),JPosition.get(5),JPosition.get(6)).setJointVelocityRel(0.5));
 		
-		pliers.getFrame("Sander").move(linRel(-deltaX, 0, 0).setJointVelocityRel(0.5));
+		//pliers.getFrame("Sander").move(linRel(-deltaX, 0, 0).setJointVelocityRel(0.5));
 		
-		pliers.getFrame("Sander").move(linRel(0, -deltaY, 0).setJointVelocityRel(0.5));
-		
-		robot.move(ptp(JPosition.get(0),JPosition.get(1),JPosition.get(2),JPosition.get(3),JPosition.get(4),JPosition.get(5),JPosition.get(6)).setJointVelocityRel(0.5));
-		
-		pliers.getFrame("Sander").move(linRel(deltaX, deltaY, 0).setJointVelocityRel(0.5));
-		
-		robot.move(ptp(JPosition.get(0),JPosition.get(1),JPosition.get(2),JPosition.get(3),JPosition.get(4),JPosition.get(5),JPosition.get(6)).setJointVelocityRel(0.5));
+		//pliers.getFrame("Sander").move(linRel(0, -deltaY, 0).setJointVelocityRel(0.5));
+				
+		//robot.move(ptp(JPosition.get(0),JPosition.get(1),JPosition.get(2),JPosition.get(3),JPosition.get(4),JPosition.get(5),JPosition.get(6)).setJointVelocityRel(0.5));
 		
 		//pliers.getFrame("Sander").move(linRel(-deltaX, -deltaY, 0).setJointVelocityRel(0.5));
 		
@@ -299,9 +295,9 @@ public class HoldAndDo extends RoboticsAPIApplication {
 		//Tant que le point en diagonale n'est pas atteint
 		//Aller-retour puis décalage 
 		do{
+			pliers.getFrame("Sander").move(linRel(-deltaX, -deltaY, 0).setJointVelocityRel(0.5));
 			pliers.getFrame("Sander").move(linRel(deltaX, deltaY, 0).setJointVelocityRel(0.5));
-			pliers.getFrame("Sander").move(linRel(deltaX, deltaY, 0).setJointVelocityRel(0.5));
-			pliers.getFrame("Sander").move(linRel(deltaX2 / decalage, deltaY2 / decalage, 0).setJointVelocityRel(0.5));
+			pliers.getFrame("Sander").move(linRel(-deltaX2 / decalage, -deltaY2 / decalage, 0).setJointVelocityRel(0.5));
 			//Incrémentation de la variable d'arrêt
 			varX += deltaX2 / decalage;
 		}while(varX < refThirdPoint.getX());
