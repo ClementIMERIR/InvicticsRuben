2eb1d1fe26100566db2434240d336343c8f5ce2d
rForcee <erwannfrancois@gmail.com>
2018-03-22 15:14:41 +0100

    --- automatic rebase to remote; old local SHA: 4f20bd094ec80b81ef76a5245e13d822d208c417 ---

--- src/application/HoldAndDo.java
+++ src/application/HoldAndDo.java
@@ -280,9 +280,9 @@ public class HoldAndDo extends RoboticsAPIApplication {
 		//Tant que le point en diagonale n'est pas atteint
 		//Aller-retour puis décalage 
 		do{
-			pliers.getFrame("Sander").move(linRel(deltaX, -deltaY, 0).setJointVelocityRel(0.5));
-			pliers.getFrame("Sander").move(linRel(-deltaX, deltaY, 0).setJointVelocityRel(0.5));
-			pliers.getFrame("Sander").move(linRel(deltaX2 / decalage, -deltaY2 / decalage, 0).setJointVelocityRel(0.5));
+			pliers.getFrame("Sander").move(linRel(-deltaX, -deltaY, 0).setJointVelocityRel(0.5));
+			pliers.getFrame("Sander").move(linRel(deltaX, deltaY, 0).setJointVelocityRel(0.5));
+			pliers.getFrame("Sander").move(linRel(deltaX2 / decalage, deltaY2 / decalage, 0).setJointVelocityRel(0.5));
 			//Incrémentation de la variable d'arrêt
 			varX += deltaX2 / decalage;
 		}while(varX < refThirdPoint.getX());
