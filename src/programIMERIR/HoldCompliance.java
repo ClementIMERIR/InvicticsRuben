d8c082da677d1a4eaae500ec78ea8eb516482e5c
dmBontemps <dmbontemps@github.com>
2018-03-22 13:21:42 +0100

    --- automatic rebase to remote; old local SHA: 030e19da074aeb099f46ed84235a3b2c0e43d7b0 ---

--- src/programIMERIR/HoldCompliance.java
+++ src/programIMERIR/HoldCompliance.java
@@ -131,7 +131,7 @@ public class HoldCompliance extends RoboticsAPIApplication {
 				currentFrame = robot.getCurrentCartesianPosition(penWorldAlign);
 				altitude = currentFrame.getZ();
 				getLogger().info("Altitude = "+altitude);
-				penCollisionObserver.disable();
+				//penCollisionObserver.disable();
 				penWorldAlign.move(linRel(200, 200, 0).setJointVelocityRel(40).setMode(drawMode));
 				//drawSquare(currentFrame.getX(), currentFrame.getY(), squareSize);
 				penWorldAlign.move(ptp(getApplicationData().getFrame("/WorkingTable/P6")));
