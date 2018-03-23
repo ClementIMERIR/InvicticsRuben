46733580bba4ac08a1e82aab0fd3068dc6a8bcf2
oufleure <oufleure@DESKTOP-G4RFU30>
2018-03-23 13:07:18 +0100

    --- automatic rebase to remote; old local SHA: 63492d8f3b841d5f3752ae797917063c0f0ada60 ---

--- src/programIMERIR/TrainingLegs_final.java
+++ src/programIMERIR/TrainingLegs_final.java
@@ -224,10 +224,10 @@ public class TrainingLegs_final extends RoboticsAPIApplication {
 		while (answer2 != 2) {
 			if (answer2 == 0) {
 				legLift.getFrame("/TCP").move(
-						ptp(getApplicationData().getFrame("/Genoux/P1")));
+						ptp(getApplicationData().getFrame("/Genoux/P1")).setJointAccelerationRel(0.5));
 			} else {
 				legLift.getFrame("/TCP").move(
-						ptp(getApplicationData().getFrame("/Genoux/P2")));
+						ptp(getApplicationData().getFrame("/Genoux/P2")).setJointAccelerationRel(0.5));
 			}
 			answer2 = -1;
 
